da4a0408f3766da3135887e5572cbc8d0c35b068
rmentake <rembespolone@gmail.com>
2025-12-01 21:21:27 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,8 @@ export default {
 
 		const staticFiles = [
 			"favicon.ico",
-			"ajax/libs/jquery/3.6.0/jquery.min.js"
+			"ajax/libs/jquery/3.6.0/jquery.min.js",
+			"ajax/libs/jquery/3.6.1/jquery.min.js"
 		];
 
 		if (staticFiles.includes(pathname.slice(1))) {
